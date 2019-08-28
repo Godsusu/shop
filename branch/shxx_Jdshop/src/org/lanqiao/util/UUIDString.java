d2ab981383086f51b4dd93d869f125b4c1a42e2f
shixuexue <shixuexue@25c25923-582a-4b2f-883e-2f85afa58443>
2019-08-28 11:59:40 +0000

    第一次导入

---
+++ branch/shxx_Jdshop/src/org/lanqiao/util/UUIDString.java
@@ -0,0 +1,10 @@
+package org.lanqiao.util;
+
+import java.util.UUID;
+
+public class UUIDString {
+
+	public static String getId(){
+		return UUID.randomUUID().toString().replace("-", "").toUpperCase().substring(0,5);
+	}
+}
