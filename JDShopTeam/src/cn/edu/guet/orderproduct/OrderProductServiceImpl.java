235aaa702940f4e62158ef8114fd194ff8d41f01
team <team@25c25923-582a-4b2f-883e-2f85afa58443>
2019-09-01 04:10:53 +0000

    下订单成功，下订单后清理购物车，页面未读取到Sessin自动返回登录界面

--- JDShopTeam/src/cn/edu/guet/orderproduct/OrderProductServiceImpl.java
+++ JDShopTeam/src/cn/edu/guet/orderproduct/OrderProductServiceImpl.java
@@ -1,16 +1,22 @@
 package cn.edu.guet.orderproduct;
 
+import java.sql.Connection;
 import java.sql.SQLException;
+
+import org.lanqiao.util.DBConnection;
 
 public class OrderProductServiceImpl implements IOrderProductService {
 	IOrderProductDao orderProductDao=new OrderProductDaoImpl();
-
+	Connection conn=null;
 	@Override
 	public void save(OrderProduct orderProduct) {
 		try {
+			conn=DBConnection.getConn();
 			orderProductDao.save(orderProduct);
 		} catch (SQLException e) {
 			e.printStackTrace();
+		}finally{
+			DBConnection.closeConn();
 		}
 		
 	}
